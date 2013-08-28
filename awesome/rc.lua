c39fa566e0e533385c7a1e1ce7ed9b0c25066a5f
yawnt <yawn.localhost@gmail.com>
2013-08-28 21:11:27 +0200

    stuff

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -151,14 +151,6 @@ for s = 1, screen.count() do
 end
 -- }}}
 
--- {{{ Mouse bindings
-root.buttons(awful.util.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
--- }}}
-
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
