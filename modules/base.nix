712b0f4e8002ecdd7d7c96201dfd698f7d293a96
yawnt <me@yawnt.com>
2016-05-07 15:25:48 +0200

    fix: restore aliases

--- modules/base.nix
+++ modules/base.nix
@@ -15,6 +15,11 @@ rec {
 
   time.timeZone = "Europe/Rome";
 
+  environment.shellAliases = {
+    pdfcompress = "gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -sOutputFile=$2 $1";
+    whiteboard = "convert $1 -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 $2";
+  };
+
   environment.systemPackages = with pkgs; [
     # Network
     wget
