a21947eb589e5f22a5dcac7a036e404bb692a666
yawnt <me@yawnt.com>
2016-05-07 15:43:04 +0200

    fix: remove duplication

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,7 @@ rec {
     installPhase = builtins.replaceStrings ["for file in $out/*"] ["shopt -s extglob\nfor file in $out/!(*src.zip)"] pkgs.oraclejdk8.installPhase;
   });
   sbt = pkgs.sbt.override { jre = pkgs.oraclejre8; };
-  leiningen = pkgs.leiningen.override { jdk = pkgs.oraclejdk8; };
+  leiningen = pkgs.leiningen.override { jdk = oraclejdk8; };
   apacheKafka = pkgs.apacheKafka.override { jre = pkgs.oraclejre8; };
   emacs = (import ./emacs) pkgs.emacs pkgs.fetchgit;
 }
